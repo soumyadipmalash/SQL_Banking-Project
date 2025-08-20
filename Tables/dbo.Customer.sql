d861fa35c282040d84e12565f16275826679c0bf
Soumyadip Malash <soumyadipmalash@gmail.com>
2025-08-20 20:08:48 +0530

    Initial Commit

---
+++ Tables/dbo.Customer.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Customer] (
+  [Acno] [int] IDENTITY,
+  [Name] [nvarchar](30) NULL,
+  [Address] [nvarchar](100) NULL,
+  [Phone] [nvarchar](15) NULL,
+  [Email] [nvarchar](80) NULL,
+  [Aadhar_No] [nvarchar](20) NULL,
+  [Acc_Type] [nvarchar](20) NULL,
+  [Status] [nvarchar](15) NULL,
+  [Balance] [decimal](15, 2) NULL,
+  PRIMARY KEY CLUSTERED ([Acno])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.Transaction.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Transaction] (
+  [Tid] [int] IDENTITY,
+  [Dot] [date] NULL,
+  [Amount] [decimal](15, 2) NULL,
+  [Type] [nvarchar](20) NULL,
+  [Acno] [int] NULL,
+  PRIMARY KEY CLUSTERED ([Tid])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Transaction]
+  ADD CONSTRAINT [FK_Transaction_Customer] FOREIGN KEY ([Acno]) REFERENCES [dbo].[Customer] ([Acno])
+GO
